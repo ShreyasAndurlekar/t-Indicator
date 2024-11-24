92d55cc873aeff79d05a5e14ef2286022aa89e60
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-11-24 18:18:29 +0530

    Change control flow

--- components/Account.jsx
+++ components/Account.jsx
@@ -1,134 +1,134 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView,StyleSheet,View,Text,TextInput,Button,Image} from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, TextInput, Button, Image } from 'react-native';
 import { createAccount, signIn, signOut } from '../functions/database';
 import { useNavigation } from '@react-navigation/native';
-import alert from './Alert'
+import alert from './Alert';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Account = () => {
-
     const navigation = useNavigation();
-
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [storedUsername, setStoredUsername] = useState(null);
+    const [currentPage, setCurrentPage] = useState('login'); // Tracks the active page
 
     useEffect(() => {
-        
         async function userStatus() {
-        
             const extractuser = await AsyncStorage.getItem('Username');
-            setStoredUsername(extractuser)
-
+            setStoredUsername(extractuser);
         }
-
-    userStatus()
-
-    },[])
+        userStatus();
+    }, []);
 
     const handleSubmit = async () => {
-
         if (username === '' || password === '') {
-
             alert('Error', 'Username and password are required');
             return;
         }
 
         const newAccount = { username, password };
-        
         try {
-
             const createdAccount = await createAccount(newAccount);
             setUsername('');
             setPassword('');
-
+            alert('Success', 'Account created successfully', [{ text: 'OK', onPress: () => setCurrentPage('login') }]);
         } catch (error) {
-
             console.error('Error creating account:', error);
-
-            alert('Error creating account', 'Similar username already exists', [
-                            
-                {text: 'OK', onPress: () => console.log('')},
-              
-            ]);
+            alert('Error creating account', 'Similar username already exists', [{ text: 'OK' }]);
         }
     };
 
     const handleSignIn = async () => {
-
         if (username === '' || password === '') {
-
             alert('Error', 'Username and password are required');
             return;
         }
 
         try {
-
             const userData = await signIn({ username, password });
+            setStoredUsername(username);
             navigation.navigate('Home');
-            
         } catch (error) {
-        
             console.error('Error signing in:', error);
-
-            alert('Error signing in:', 'Non-existent Password/User', [
-                            
-                {text: 'OK', onPress: () => console.log('')},
-              
-            ]);
+            alert('Error signing in:', 'Non-existent Password/User', [{ text: 'OK' }]);
         }
     };
 
-    const handleLogout = async() => {
+    const handleLogout = async () => {
+        signOut();
+        setStoredUsername(null);
+        setCurrentPage('login');
+    };
 
-        signOut()
-        navigation.navigate("Home")
-    }
-
-    return storedUsername == null ? (
+    return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.sectionContainer}>
-                <Text style={[styles.sectionTitle, { fontSize: 24 }]}>Welcome to TMT!</Text>
-                <Image
-                    style={{ width: 125, height: 125 }}
-                    source={require('../assets/adaptive-icon.png')}
-                />
-                <Text style={[styles.sectionTitle, { fontSize: 18, marginBottom: 20 }]}>Become a member</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Username"
-                    value={username}
-                    onChangeText={setUsername}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="Password"
-                    secureTextEntry
-                    value={password}
-                    onChangeText={setPassword}
-                />
-                <View style={[styles.b, { marginTop: 10 }]}>
-                    <Button title="Sign Up" onPress={handleSubmit} />
+            {storedUsername ? (
+                // Profile Page
+                <View style={styles.sectionContainer}>
+                    <Text style={[styles.sectionTitle, { fontSize: 20, marginBottom: 40 }]}>
+                        Thank You for using TMT App, {storedUsername}
+                    </Text>
+                    <Button title="Log Out" color="#FF0000" onPress={handleLogout} />
                 </View>
-                <Text style={[styles.sectionTitle, { fontSize: 14, marginTop: 30, marginBottom: 20 }]}>
-                    Already a member?
-                </Text>
-                <View style={styles.b}>
-                    <Button title="Sign In" onPress={handleSignIn} />
+            ) : (
+                <View style={styles.sectionContainer}>
+                    <Text style={[styles.sectionTitle, { fontSize: 24 }]}>Welcome to TMT!</Text>
+                    <Image
+                        style={{ width: 125, height: 125 }}
+                        source={require('../assets/adaptive-icon.png')}
+                    />
+                    {currentPage === 'signup' && (
+                        <>
+                            <Text style={[styles.sectionTitle, { fontSize: 18, marginBottom: 20 }]}>
+                                Create a new account
+                            </Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Username"
+                                value={username}
+                                onChangeText={setUsername}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Password"
+                                secureTextEntry
+                                value={password}
+                                onChangeText={setPassword}
+                            />
+                            <View style={styles.b}>
+                                <Button title="Sign Up" onPress={handleSubmit} />
+                            </View>
+                            <Button title="Login" onPress={() => setCurrentPage('login')} />
+                        </>
+                    )}
+                    {currentPage === 'login' && (
+                        <>
+                            <Text style={[styles.sectionTitle, { fontSize: 18, marginBottom: 20 }]}>
+                                Login to your account
+                            </Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Username"
+                                value={username}
+                                onChangeText={setUsername}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Password"
+                                secureTextEntry
+                                value={password}
+                                onChangeText={setPassword}
+                            />
+                            <View style={styles.b}>
+                                <Button title="Sign In" onPress={handleSignIn} />
+                            </View>
+                            <Button title="Sign Up" onPress={() => setCurrentPage('signup')} />
+                        </>
+                    )}
                 </View>
-            </View>
-        </SafeAreaView>
-    ) : (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.sectionContainer}>
-                <View style={styles.b}>
-                    <Text style={[styles.sectionTitle, { fontSize: 20, marginBottom: 40 }]}>Thank You for using TMT App</Text>
-                    <Button title="Log Out" color = "#FF0000" onPress={handleLogout} />
-                </View>
-            </View>
+            )}
         </SafeAreaView>
     );
-    
 };
 
 const styles = StyleSheet.create({
@@ -136,13 +136,13 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#f8f8f8',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     sectionContainer: {
         marginTop: 10,
         paddingHorizontal: 24,
         width: 600,
-        alignItems: 'center'
+        alignItems: 'center',
     },
     sectionTitle: {
         fontWeight: '600',
@@ -155,14 +155,12 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         marginBottom: 12,
         padding: 8,
-        width: 200
+        width: 200,
     },
-    b:{
+    b: {
         width: 100,
         marginBottom: 10,
-    }
+    },
 });
-
-
 
 export default Account;
--- components/Route.jsx
+++ components/Route.jsx
@@ -1,136 +1,126 @@
 import { Text, View, ScrollView, Pressable } from "react-native";
 import styles from '../stylesheets/global'
 import BottomBar from "./Bottom";
-import {useState, useContext, useEffect} from 'react'
+import { useState, useContext, useEffect } from 'react';
 import { BusContext } from "../functions/bus";
 import Bus from "./Bus";
-import calculateTimestamps from "../functions/extra.js"    
-import { getTime, getNearestLoc } from "../functions/database"
+import calculateTimestamps from "../functions/extra.js";
+import { getTime, getNearestLoc } from "../functions/database";
 import * as Location from 'expo-location';
-import alert from '../components/Alert'
-
+import alert from '../components/Alert';
 
 const Route = () => {
-
-    const {busStops} = useContext(BusContext)   // busStops retrieves the respective array of bus stops 
-    const {busStop} = useContext(BusContext)    // Contains the current location of the bus if retrieved
-    const {eta} = useContext(BusContext)        // Contains the eta
-    const {changeBusStop, setETA} = useContext(BusContext)
-    // {} is used to destructure the object
-  
-  const [busstopcount, setBusstopcount] = useState(0);
-  const [arrayOfTimeStamps, setArrayOfTimeStamps] = useState([]);
+    const { busStops } = useContext(BusContext);
+    const { busStop } = useContext(BusContext);
+    const { eta } = useContext(BusContext);
+    const { changeBusStop, setETA } = useContext(BusContext);
     
+    const [busstopcount, setBusstopcount] = useState(0);
+    const [arrayOfTimeStamps, setArrayOfTimeStamps] = useState([]);
+    const [showAlert, setShowAlert] = useState(false); // State to trigger alert
+
     useEffect(() => {
+        if (eta) {
+            let tempBusstopcount = 0;
+            busStops.map((stop, idx) => {
+                if (stop === busStop) {
+                    tempBusstopcount = idx;
+                }
+            });
 
-    if (eta) {
-        
-        let tempBusstopcount = 0;
-        
-        busStops.map((stop, idx) => {
-        
-        if (stop === busStop) {
-            tempBusstopcount = idx;
+            setBusstopcount(tempBusstopcount);
+            const timestamps = calculateTimestamps(eta, busStops.length - tempBusstopcount - 1);
+            setArrayOfTimeStamps(timestamps);
+
+            console.log("Immediate timestamps:", timestamps);
         }
-        });
-
-        setBusstopcount(tempBusstopcount);
-        const timestamps = calculateTimestamps(eta, busStops.length - tempBusstopcount - 1);
-        setArrayOfTimeStamps(timestamps);
-        
-        console.log("Immediate timestamps:", timestamps);
-    }
     }, [eta]);
 
-    // React cannot update a component while rendering another component "Contribute",
-    // Seems to re-render this component everytime I switch to Chat.jsx // Contribute.jsx
-    // Need more explaination
-
     const getLocation = async () => {
-
-        console.log("Ok it got triggered")
-              
+        console.log("getLocation triggered");
         let { status } = await Location.requestForegroundPermissionsAsync();
-              
+
         if (status !== 'granted') {
-              
             console.error('Permission to access location was denied');
             return;
-              
         }
-              
+
         let location = await Location.getCurrentPositionAsync({});
-        const newloc = location.coords.latitude + ", " +  location.coords.longitude
-        const nearestLocBody = await getNearestLoc(newloc)
-              
-        setETA(nearestLocBody.timeAndDistance)
-                
-        const nearestLoc = nearestLocBody.nearest
-        let ok = 0
-              
-        for(var i = 0; i < busStops.length; i++){
-              
-            if(nearestLoc == busStops[i])
-                ok = 1
+        const newloc = `${location.coords.latitude}, ${location.coords.longitude}`;
+        const nearestLocBody = await getNearestLoc(newloc);
+
+      
+
+        const nearestLoc = nearestLocBody.nearest;
+        let ok = 0;
+
+        for (var i = 0; i < busStops.length; i++) {
+            if (nearestLoc === busStops[i]) ok = 1;
         }
-              
-        if(ok == 0){
-              
+
+        if (ok === 0) {
             alert('TMT is restricted to Thane only!', `Nearest bus stop is ${nearestLoc}`, [
-                                
-                    {text: 'OK', onPress: () => console.log('')},
-                      
+                { text: 'OK', onPress: () => console.log('') },
             ]);
-                }
-        else
-            changeBusStop(nearestLoc)
-              
-    }
-              
-    return(
-        <View style = {styles.root}>
-            <ScrollView style = {styles.sv}>
-            {
-            busStops.map((stop, idx) => (
-
-                <View key = {idx} style = {{flexDirection: 'row'}}>
-
-                    <View style = {styles.buspath}>
-
-                        {stop === busStop && <Bus/>}
-
-                    </View>
-
-                    <Pressable  onPress={async () => {
-                            
-                            changeBusStop(stop)
-                            const eta_ = await getTime(stop)
-                            setETA(eta_)
-
-                       }} style = {styles.busstop}>
-
-                        <Text style = {styles.bstext}>
-                            {stop}
-                        </Text>
-                        <Text>{ 
-                        
-
-                        idx >= busstopcount + 1 ? arrayOfTimeStamps[idx - busstopcount - 1] : ''
-                          
-                        }</Text> 
-                    </Pressable>
-
-                </View>
-            ))
-            
+            return
+        } else {
+            changeBusStop(nearestLoc);
         }
-            </ScrollView>
-       
-        <BottomBar />
-        
-        </View>
-       
-          )
-}
 
-export default Route
+        setETA(nearestLocBody.timeAndDistance);
+    };
+
+    useEffect(() => {
+        // Delay the alert until after the component is rendered
+        const timer = setTimeout(() => setShowAlert(true), 500);
+        return () => clearTimeout(timer); // Cleanup timeout on unmount
+    }, []);
+
+    useEffect(() => {
+        if (showAlert) {
+            alert(
+                'Are you in the bus right now ?',
+                'Pressing OK will use your location to track bus',
+                [
+                    {
+                        text: 'OK',
+                        onPress: () => {
+                            setShowAlert(false); // Dismiss the alert
+                            getLocation(); // Trigger location retrieval
+                        },
+                    },
+                ]
+            );
+        }
+    }, [showAlert]);
+
+    return (
+        <View style={styles.root}>
+            <ScrollView style={styles.sv}>
+                {busStops.map((stop, idx) => (
+                    <View key={idx} style={{ flexDirection: 'row' }}>
+                        <View style={styles.buspath}>{stop === busStop && <Bus />}</View>
+
+                        <Pressable
+                            onPress={async () => {
+                                changeBusStop(stop);
+                                const eta_ = await getTime(stop);
+                                setETA(eta_);
+                            }}
+                            style={styles.busstop}
+                        >
+                            <Text style={styles.bstext}>{stop}</Text>
+                            <Text>
+                                {idx >= busstopcount + 1 ? arrayOfTimeStamps[idx - busstopcount - 1] : ''}
+                            </Text>
+                        </Pressable>
+                    </View>
+                ))}
+            </ScrollView>
+
+            <BottomBar />
+        </View>
+    );
+};
+
+export default Route;
